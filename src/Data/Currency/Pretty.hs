7ef1e65bbe19e738abefe6e452d4b08e48290923
Daniel Campoverde <alx@sillybytes.net>
2017-08-18 14:39:06 -0500

    Additional printing configuration fields

--- src/Data/Currency/Pretty.hs
+++ src/Data/Currency/Pretty.hs
@@ -15,8 +15,18 @@ humanReadableWith cnf amount = pack $ show amount
 defaultConfig :: PrettyConfig
 defaultConfig = PrettyConfig
     { showFractions = True
+    , separateFourDigitAmounts = False
+    , useCurrencySymbol = False
+    , suffixIsoCode = False
+    , largeAmountSeparator = ','
+    , decimalSeparator = '.'
     }
 
 data PrettyConfig = PrettyConfig
     { showFractions :: Bool
+    , separateFourDigitAmounts :: Bool
+    , useCurrencySymbol :: Bool
+    , suffixIsoCode :: Bool
+    , largeAmountSeparator :: Char
+    , decimalSeparator :: Char
     } deriving (Show)
