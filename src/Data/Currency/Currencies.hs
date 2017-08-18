0eae5332c4ca53e035070295f864c497a20fd2b8
Daniel Campoverde <alx@sillybytes.net>
2017-08-18 13:59:11 -0500

    Include currency types

--- src/Data/Currency/Currencies.hs
+++ src/Data/Currency/Currencies.hs
@@ -13,19 +13,24 @@ module Data.Currency.Currencies
     , USD(..)
     ) where
 
-data Amount a = Amount a Double
+-- | Monetary amounts
+data Amount a = Amount a Double deriving (Show, Read, Eq, Ord) -- FIXME: Ord manual instance
 
 -- | Create a monetary amount of a particular currency
 amount :: (IsCurrency c) => c -> Double -> Amount c
 amount = Amount
 
+data CurrencyType = Circulating | Local | Fictional deriving (Show, Read, Eq)
+
 class IsCurrency a where
+    currencyType :: CurrencyType
     -- | ISO 4217 Currency Code
     isoCode :: a -> String
     -- | ISO 4217 Currency Numeric Code
     isoNumericCode :: String
     -- | Number of digits after the decimal separator
     decimalDigits :: Int
+
     exchangeUSD :: Float
     -- | ISO 3166-1 alpha-2 Country codes where the currency is used
     countries :: [String]
@@ -37,6 +42,7 @@ data USD = USD deriving (Show)
 data EUR = EUR deriving (Show)
 
 instance IsCurrency USD where
+    currencyType = Circulating
     isoCode = show
     isoNumericCode = "840"
     decimalDigits = 2
@@ -45,6 +51,7 @@ instance IsCurrency USD where
         , "FM", "MP", "PW", "PA", "PR", "TL", "TC", "VI"]
 
 instance IsCurrency EUR where
+    currencyType = Circulating
     isoCode = show
     isoNumericCode = "978"
     decimalDigits = 2
---
+++ src/Data/Currency/Pretty.hs
@@ -0,0 +1,1 @@
+module Data.Currency.Pretty where
