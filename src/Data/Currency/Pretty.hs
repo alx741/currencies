4740cb30cb8b9008c6fd3c961ad4516dfa2b7c44
Daniel Campoverde <alx@sillybytes.net>
2017-08-18 14:19:38 -0500

    Pretty printing skeleton

--- src/Data/Currency/Pretty.hs
+++ src/Data/Currency/Pretty.hs
@@ -1,1 +1,22 @@
 module Data.Currency.Pretty where
+
+import Data.Text
+import Data.Currency.Currencies
+
+-- | Pretty print a monetary amount using 'defaultConfig'
+humanReadable :: (IsCurrency c) => Amount c -> Text
+humanReadable = humanReadableWith defaultConfig
+
+-- | Pretty print a monetary amount with a custom configuration
+humanReadableWith :: (IsCurrency c) => PrettyConfig -> Amount c -> Text
+humanReadableWith cnf amount = pack $ show amount
+
+
+defaultConfig :: PrettyConfig
+defaultConfig = PrettyConfig
+    { showFractions = True
+    }
+
+data PrettyConfig = PrettyConfig
+    { showFractions :: Bool
+    } deriving (Show)
