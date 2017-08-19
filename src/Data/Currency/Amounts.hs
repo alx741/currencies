3bf6ea5d491ec28f674c787cbddcc23baa5c720c
Daniel Campoverde <alx@sillybytes.net>
2017-08-19 17:44:53 -0500

    Constraint Amounts to Currency type class

--- src/Data/Currency/Amounts.hs
+++ src/Data/Currency/Amounts.hs
@@ -16,7 +16,7 @@ module Data.Currency.Amounts
 import Data.Currency.Currencies
 
 -- | Monetary amounts
-data Amount c = Amount c Double deriving (Show, Read, Eq)
+data (Currency c) => Amount c = Amount c Double deriving (Show, Read, Eq)
 
 instance (Currency c) => Ord (Amount c) where
     (Amount _ v1) <= (Amount _ v2) = v1 <= v2
